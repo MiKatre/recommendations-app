6d478c27923f633747b0f127b0eb37110c594296
katre <0mica4420@gmail.com>
2019-10-18 20:39:30 +0200

    Initial commit: Kickstart the project

---
+++ screens/HomeScreen.js
@@ -0,0 +1,56 @@
+import * as WebBrowser from 'expo-web-browser';
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import { MonoText } from '../components/StyledText';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={styles.contentContainer}>
+
+        <Text style={styles.title}>Regarder </Text>
+
+      </ScrollView>
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+
+function handleLearnMorePress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/development-mode/'
+  );
+}
+
+function handleHelpPress() {
+  WebBrowser.openBrowserAsync(
+    'https://docs.expo.io/versions/latest/workflow/up-and-running/#cant-see-your-changes'
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 34,
+    fontWeight: 'bold',
+    margin: 20,
+  },
+});
